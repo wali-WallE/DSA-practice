65d95f3c4f896be96df264c97a0146f1a84305c2
Abdul Wali <waliwmee78@gmail.com>
2025-12-26 22:19:26 +0500

    DSA practice: deletion at some specific node in linked list

---
+++ linked-list/deleting-middle/tempCodeRunnerFile.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+int main (){
+    Node* START = NULL;
+    Node* TEMP;
+    int pos;
+    Node* n1 = new Node{10, NULL};
+    Node* n2 = new Node{20, NULL};
+    Node* n3 = new Node{30, NULL};
+    Node* n4 = new Node{40, NULL};
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = NULL;
+    START = n1;
+
+    cout << "enter positon u want to delete ";
+    cin >> pos;
+
+    if( START == NULL ){
+        cout << "empty list nothing to be deleted";
+        return 0;
+    }
+    else if(pos == 0){
+        TEMP = START;
+        START->next = NULL;
+    }
+    else{
+        TEMP = START;
+        for(int i = 0; i < pos - 1 && TEMP!= NULL; i++){
+            TEMP = TEMP->next;
+        }
+        Node* del = TEMP->next;
+        cout << "deleting node at " << pos << ": " << del->data << endl;
+        TEMP->next = del->next; 
+    }
+
+    TEMP=START;
+    cout << "List : ";
+    while (TEMP != NULL){
+        cout << TEMP->data << "-> ";
+        TEMP = TEMP->next;
+    }
+    cout << "NULL" << endl;
+    
+    return 0;
+}
